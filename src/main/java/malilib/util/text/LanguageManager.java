b7b82e929843ca0a1008fb9aeb6bd3a17dabf2b7
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2024-09-14 22:52:02 +0300

    LanguageManager: Support other mods as well

--- src/main/java/malilib/util/text/LanguageManager.java
+++ src/main/java/malilib/util/text/LanguageManager.java
@@ -3,6 +3,7 @@ package malilib.util.text;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.HashSet;
 import java.util.Properties;
 
 import malilib.MaLiLib;
@@ -12,23 +13,42 @@ import malilib.util.FileUtils;
 public class LanguageManager
 {
     public static final LanguageManager INSTANCE = new LanguageManager();
+
+    protected final HashSet<String> modIds = new HashSet<>();
     protected final Properties translations = new Properties();
 
     protected LanguageManager()
     {
-        this.reload();
+        this.registerMod(MaLiLibReference.MOD_ID);
+    }
+
+    public void registerMod(String modId)
+    {
+        if (this.modIds.add(modId))
+        {
+            this.loadTranslation(modId);
+        }
+    }
+
+    protected void loadTranslation(String modId)
+    {
+        try
+        {
+            this.translations.load(new InputStreamReader(FileUtils.openModResource(modId, "/lang/en_us.lang"), StandardCharsets.UTF_8));
+        }
+        catch (IOException e)
+        {
+            MaLiLib.LOGGER.error("Exception trying to load translations for mod {}: ", modId, e);
+        }
     }
 
     public void reload()
     {
-        try
+        this.translations.clear();
+
+        for (String modId : this.modIds)
         {
-            this.translations.clear();
-            this.translations.load(new InputStreamReader(FileUtils.openModResource(MaLiLibReference.MOD_ID, "/lang/en_us.lang"), StandardCharsets.UTF_8));
-        }
-        catch (IOException e)
-        {
-            MaLiLib.LOGGER.error("Exception trying to load translations: ", e);
+            this.loadTranslation(modId);
         }
     }
 
